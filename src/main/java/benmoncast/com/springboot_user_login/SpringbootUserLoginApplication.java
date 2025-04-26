f270be6e50770867ed37321f12e60af7831dcbd0
Benedict Castro <benmoncast@gmail.com>
2025-04-26 15:04:35 +0800

    Initial commit

---
+++ src/main/java/benmoncast/com/springboot_user_login/SpringbootUserLoginApplication.java
@@ -0,0 +1,13 @@
+package benmoncast.com.springboot_user_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUserLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/benmoncast/com/springboot_user_login/SpringbootUserLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package benmoncast.com.springboot_user_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
